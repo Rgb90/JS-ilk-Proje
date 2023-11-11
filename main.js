bd2783cea30f82e7dc9c50c0ac91718c4db2eb7a
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2023-11-12 00:11:31 +0300

    düzeltme

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 function clickCounter() {
-    {
-     if (sessionStorage.clickcount) {
-       sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
-     } else {
-       sessionStorage.clickcount = 1;
-     }
-     document.getElementById("result").innerHTML = sessionStorage.clickcount;
-   } 
- }
- 
+  {
+    if (sessionStorage.clickcount) {
+      sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+    } else {
+      sessionStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = sessionStorage.clickcount;
+  }
+}
+
+/* iki farklı sayaç */
+
+let counter = 0;
+
+function myFunction() {
+    counter ++;
+    document.getElementById("counter").innerHTML = counter;
+}
+
+/* sadece bu şekilde de oluyor; 
+let counter = 0;
+function myFunction() {
+    document.getElementById("counter").innerHTML  ++;
+} */
+
+
+/* const password = prompt("parola");
+const passwordConfirmation = prompt("tekrar girin");
+
+const passwordsDidNotMatch = password != passwordConfirmation;
+
+if (passwordsDidNotMatch) {
+  alert("Parola uyşumadı");
+}
+  else {
+    console.log("Geçerli parola");
+  }
+
+ */
